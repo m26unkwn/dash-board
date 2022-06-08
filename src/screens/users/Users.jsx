78bcb7829c3bc6236a33ab5e7c82aa943709260d
m26unkwn <getmonushukla@gmail.com>
2022-06-08 18:24:59 +0530

    Table: styling added

--- src/screens/users/Users.jsx
+++ src/screens/users/Users.jsx
@@ -1,5 +1,5 @@
-import { Input } from "../../components";
-import { searchIcon } from "../../assets";
+import { Input, PrimaryButton } from "../../components";
+import { searchIcon, FilterIcon, AddIcon, Avatar } from "../../assets";
 import "./users.css";
 
 export const Users = () => {
@@ -7,15 +7,62 @@ export const Users = () => {
     <div className='account users-container'>
       <div className='users-header'>
         <h2 className='users-title'>User Records</h2>
-        <div className='users-filtes'>
+        <div className='users-filters'>
           <div className='user-search'>
             <Input placholder='Search in table...' type='text' />
             <button>
               <img src={searchIcon} alt='Search-user' />
             </button>
           </div>
+          <button className='filter-btn'>
+            <img src={FilterIcon} alt='filter' />
+            <span>Filter</span>
+          </button>
+          <button className='btn add-btn'>
+            <img src={AddIcon} alt='add' />
+            <span>Add</span>
+          </button>
         </div>
       </div>
+      <table>
+        <tr>
+          <td>
+            <img src={Avatar} />
+          </td>
+          <td>Robert Thomas</td>
+          <td>robert.thomas@gmail.com</td>
+          <td>Male</td>
+          <td>Admin</td>
+        </tr>
+        <tr>
+          <td>
+            <img src={Avatar} />
+          </td>
+          <td>Robert Thomas</td>
+          <td>robert.thomas@gmail.com</td>
+          <td>Male</td>
+          <td>Admin</td>
+        </tr>
+        <tr>
+          <td>
+            <img src={Avatar} />
+          </td>
+          <td>Robert Thomas</td>
+          <td>robert.thomas@gmail.com</td>
+          <td>Male</td>
+          <td>Admin</td>
+        </tr>
+        <tr>
+          <td>
+            <img src={Avatar} />
+          </td>
+          <td>Robert Thomas</td>
+          <td>robert.thomas@gmail.com</td>
+          <td>Male</td>
+          <td>Admin</td>
+        </tr>
+      </table>
+      <div className='pagination'></div>
     </div>
   );
 };

d710b19243fd70068ed9353810c95c2104ef3d3c
m26unkwn <getmonushukla@gmail.com>
2022-06-15 00:30:21 +0530

    responsive : error page responsive

--- src/screens/sidebar/Sidebar.jsx
+++ src/screens/sidebar/Sidebar.jsx
@@ -1,11 +1,8 @@
 import "./sidebar.css";
-import { NavLink } from "react-router-dom";
-import { navItems } from "../../data";
-import { useAuth } from "../../store/provider/auth-provider";
+
 import { SidebarNav } from "../../components";
 
 export const Sidebar = () => {
-  const { logoutUser } = useAuth();
   return (
     <div className='sidebar-container'>
       <SidebarNav />
